95ce059f3213f60c7f3558e8308157c43bcfff26
VioletZengCIAI <136747864+VioletZengCIAI@users.noreply.github.com>
2023-06-15 11:36:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const sql = require('mssql');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const config = {
+  user: 'dbusr36261',
+  password: 'w4ARyVVw**5}&#SB',
+  server: 'i4pd-db0036261-001.database.windows.net',
+  database: 'i4pd-db0036261-001-userdata',
+
+};
+
+
+// dbo.V000000677719
+
+app.get('/data', async (req, res) => {
+  try {
+    await sql.connect(config);
+    const result = await sql.query('SELECT * FROM dbo.V000000677719');
+    const dataArray = result.recordset;
+    res.send(dataArray);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error retrieving data from SQL Server.');
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/styles', express.static(path.join(__dirname, 'public')));
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
