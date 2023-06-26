31657c2b1e6fe554fe35aca40f1a09e0a2fbd116
Violetzzzzz <60115339+Violetzzzzz@users.noreply.github.com>
2023-06-26 16:43:43 -0700

    Update features

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 // // Start the server
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
